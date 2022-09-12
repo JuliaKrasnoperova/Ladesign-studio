9c59b7e9dd9ae170a10d4390e4e8b8c76429d0b7
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2022-09-12 23:53:39 +0500

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,4 +14,31 @@ accordionTitles.forEach((accordionTitle) => {
     })
 })
 
-// Tab
+// Tabs
+let tabNavItem = document.querySelectorAll('.tabs__nav-item');
+let tabContentItem = document.querySelectorAll('.tabs__content');
+
+tabNavItem.forEach(function (elem) {
+    elem.addEventListener('click', activeTab);
+})
+
+function activeTab() {
+    tabNavItem.forEach(function (elem) {
+        elem.classList.remove('active');
+    })
+    this.classList.add('active');
+    let tabName = this.getAttribute('data-tab');
+
+    activeTabContent(tabName);
+
+}
+
+function activeTabContent(tabName) {
+    tabContentItem.forEach(function (item) {
+        if (item.classList.contains(tabName)) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    })
+}
