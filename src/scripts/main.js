4df5f4f19ada79a2b635c83335310374c7eab52f
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2022-10-01 12:52:33 +0500

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,3 @@
-// Atttach files
-window.onload = () => {
-    const uploadFile = document.getElementById("form__upload-file");
-    const uploadBtn = document.getElementById("form__upload-btn");
-    const uploadText = document.getElementById("form__upload-text");
-
-    // Иммитируем нажатие по нашей скрытой кнопке закгрузке файлов
-    uploadBtn.addEventListener("click", function () {
-        uploadFile.click();
-    });
-    // Добавляем событие загрузки файла
-    uploadFile.addEventListener("change", function () {
-        if (uploadFile.value) {
-            uploadText.innerText = uploadFile.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
-        } else {
-            uploadText.innerText = "Файл не выбран";
-        }
-    });
-}
 // form placeholder
 const formItems = document.querySelectorAll('.form-item__field');
 
@@ -36,8 +17,59 @@ for (let item of formItems) {
             thisPlaceholder.classList.remove('active');
         }
     })
-}
+};
+// Atttach files
+window.onload = () => {
+    const uploadFile = document.getElementById("form__upload-file");
+    const uploadBtn = document.getElementById("form__upload-btn");
+    const uploadText = document.getElementById("form__upload-text");
 
+    // Иммитируем нажатие по нашей скрытой кнопке закгрузке файлов
+    if (uploadBtn) {
+        uploadBtn.addEventListener("click", function () {
+            uploadFile.click();
+        });
+    }
+    // Добавляем событие загрузки файла
+    if (uploadFile) {
+        uploadFile.addEventListener("change", function () {
+            if (uploadFile.value) {
+                uploadText.innerText = uploadFile.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
+            } else {
+                uploadText.innerText = "Файл не выбран";
+            }
+        });
+    }
+};
+
+// pop-up
+let popupBg = document.querySelector('#popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('#popup__open');
+let closePopupButton = document.querySelector('.popup__close');
+let body = document.querySelector('body');
+
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('popup__active');
+        body.classList.add('locked');
+    })
+});
+closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('popup__active');
+    body.classList.remove('locked');
+});
+document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('popup__active');
+        body.classList.remove('locked');
+    }
+});
 
 // Accordion index
 const accordionTitles = document.querySelectorAll('.accordion__title');
@@ -53,7 +85,7 @@ accordionTitles.forEach((accordionTitle) => {
             accordionTitle.nextElementSibling.style.maxHeight = "0px";
         }
     })
-})
+});
 
 
 // Tabs
@@ -83,93 +115,129 @@ function activeTabContent(tabName) {
             item.classList.remove('active');
         }
     })
-}
-// Carousel
-const carousel = function () {
-    const carousel = document.querySelectorAll('.carousel__item');
-    const btnLeft = document.querySelector('.carousel__btn--left');
-    const btnRight = document.querySelector('.carousel__btn--right');
-
-    let curSlide = 0;
-    const maxSlide = carousel.length;
-
-    const goToSlide = function (slideItem) {
-        carousel.forEach(
-            (s, i) => (s.style.transform = `translateX(${100 * (i - slideItem)}%)`)
-        );
-    };
-
-    // Next slide
-    const nextSlide = function () {
-        if (curSlide === maxSlide - 1) {
-            curSlide = 0;
-        } else {
-            curSlide++;
-        }
-
-        goToSlide(curSlide);
-    };
-
-    const prevSlide = function () {
-        if (curSlide === 0) {
-            curSlide = maxSlide - 1;
-        } else {
-            curSlide--;
-        }
-        goToSlide(curSlide);
-    };
-
-    const init = function () {
-        goToSlide(0);
-    };
-    init();
-
-    // Event handlers
-    btnRight.addEventListener('click', nextSlide);
-    btnLeft.addEventListener('click', prevSlide);
-
-    document.addEventListener('keydown', function (e) {
-        if (e.key === 'ArrowLeft') prevSlide();
-        e.key === 'ArrowRight' && nextSlide();
-    });
 };
-carousel();
-
 
 // Slider
-// Устанавливаем индекс слайда по умолчанию 
-let slideIndex = 1;
-showSlides(slideIndex);
+var slider = document.getElementById('slider'),
+    sliderItems = document.getElementById('slides'),
+    prev = document.getElementById('prev'),
+    next = document.getElementById('next');
 
-// Увеличиваем индекс на 1 — показываем следующий слайд
-function nextSlide() {
-    showSlides(slideIndex += 1);
-}
+function slide(wrapper, items, prev, next) {
+    var posX1 = 0,
+        posX2 = 0,
+        posInitial,
+        posFinal,
+        threshold = 100,
+        slides = items.getElementsByClassName('slider__item'),
+        slidesLength = slides.length,
+        slideSize = items.getElementsByClassName('slider__item')[0].offsetWidth,
+        firstSlide = slides[0],
+        lastSlide = slides[slidesLength - 1],
+        cloneFirst = firstSlide.cloneNode(true),
+        cloneLast = lastSlide.cloneNode(true),
+        index = 0,
+        allowShift = true;
 
-// Уменьшает индекс на 1 — показываем предыдущий слайд
-function previousSlide() {
-    showSlides(slideIndex -= 1);
-}
+    // Clone first and last slide
+    items.appendChild(cloneFirst);
+    items.insertBefore(cloneLast, firstSlide);
+    wrapper.classList.add('loaded');
 
-// Устанавливаем текущий слайд 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+    // Mouse events
+    items.onmousedown = dragStart;
 
-// Функция перелистывания 
-function showSlides(n) {
-    let slides = document.getElementsByClassName("slider__item");
+    // Touch events
+    items.addEventListener('touchstart', dragStart);
+    items.addEventListener('touchend', dragEnd);
+    items.addEventListener('touchmove', dragAction);
 
-    if (n > slides.length) {
-        slideIndex = 1
+    // Click events
+    prev.addEventListener('click', function () {
+        shiftSlide(-1)
+    });
+    next.addEventListener('click', function () {
+        shiftSlide(1)
+    });
+
+    // Transition events
+    items.addEventListener('transitionend', checkIndex);
+
+    function dragStart(e) {
+        e = e || window.event;
+        e.preventDefault();
+        posInitial = items.offsetLeft;
+
+        if (e.type == 'touchstart') {
+            posX1 = e.touches[0].clientX;
+        } else {
+            posX1 = e.clientX;
+            document.onmouseup = dragEnd;
+            document.onmousemove = dragAction;
+        }
     }
-    if (n < 1) {
-        slideIndex = slides.length
+
+    function dragAction(e) {
+        e = e || window.event;
+
+        if (e.type == 'touchmove') {
+            posX2 = posX1 - e.touches[0].clientX;
+            posX1 = e.touches[0].clientX;
+        } else {
+            posX2 = posX1 - e.clientX;
+            posX1 = e.clientX;
+        }
+        items.style.left = (items.offsetLeft - posX2) + "px";
     }
 
-    // Проходим по каждому слайду в цикле for 
-    for (let slide of slides) {
-        slide.style.display = "none";
+    function dragEnd(e) {
+        posFinal = items.offsetLeft;
+        if (posFinal - posInitial < -threshold) {
+            shiftSlide(1, 'drag');
+        } else if (posFinal - posInitial > threshold) {
+            shiftSlide(-1, 'drag');
+        } else {
+            items.style.left = (posInitial) + "px";
+        }
+
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
-    slides[slideIndex - 1].style.display = "block";
-};
+
+    function shiftSlide(dir, action) {
+        items.classList.add('shifting');
+
+        if (allowShift) {
+            if (!action) {
+                posInitial = items.offsetLeft;
+            }
+
+            if (dir == 1) {
+                items.style.left = (posInitial - slideSize) + "px";
+                index++;
+            } else if (dir == -1) {
+                items.style.left = (posInitial + slideSize) + "px";
+                index--;
+            }
+        };
+
+        allowShift = false;
+    }
+
+    function checkIndex() {
+        items.classList.remove('shifting');
+
+        if (index == -1) {
+            items.style.left = -(slidesLength * slideSize) + "px";
+            index = slidesLength - 1;
+        }
+
+        if (index == slidesLength) {
+            items.style.left = -(1 * slideSize) + "px";
+            index = 0;
+        }
+
+        allowShift = true;
+    }
+}
+slide(slider, sliderItems, prev, next);
