d22da6dd2300ca43f1bdff6cc3bed3b45981dbfd
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-03-25 20:51:56 +0500

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,166 +1,214 @@
 // Импорт пакетов
-const gulp = require('gulp')
-const less = require('gulp-less')
-const stylus = require('gulp-stylus')
-const sass = require('gulp-sass')(require('sass'))
-const rename = require('gulp-rename')
-const cleanCSS = require('gulp-clean-css')
-const ts = require('gulp-typescript')
+const gulp = require("gulp");
+const less = require("gulp-less");
+const stylus = require("gulp-stylus");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
+const ts = require("gulp-typescript");
 //const coffee = require('gulp-coffee')
-const babel = require('gulp-babel')
-const uglify = require('gulp-uglify')
-const concat = require('gulp-concat')
-const sourcemaps = require('gulp-sourcemaps')
-const autoprefixer = require('gulp-autoprefixer')
-const imagemin = require('gulp-imagemin')
-const htmlmin = require('gulp-htmlmin')
-const size = require('gulp-size')
-const gulppug = require('gulp-pug')
-const newer = require('gulp-newer')
-const browsersync = require('browser-sync').create()
-const del = require('del')
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const htmlmin = require("gulp-htmlmin");
+const size = require("gulp-size");
+const gulppug = require("gulp-pug");
+const newer = require("gulp-newer");
+const browsersync = require("browser-sync").create();
+const del = require("del");
 
 // Пути исходных файлов src и пути к результирующим файлам dest
 const paths = {
   pug: {
-    src: ['src/*.pug'],
-    dest: 'dist/'
+    src: ["src/*.pug"],
+    dest: "dist/",
   },
   html: {
-    src: ['src/*.html'],
-    dest: 'dist/'
+    src: ["src/*.html"],
+    dest: "dist/",
   },
   styles: {
-    src: ['src/styles/**/*.sass', 'src/styles/**/*.scss', 'src/styles/**/*.styl', 'src/styles/**/*.less', 'src/styles/**/*.css'],
-    dest: 'dist/css/'
+    src: [
+      "src/styles/**/*.sass",
+      "src/styles/**/*.scss",
+      "src/styles/**/*.styl",
+      "src/styles/**/*.less",
+      "src/styles/**/*.css",
+    ],
+    dest: "dist/css/",
   },
   scripts: {
-    src: ['src/scripts/**/*.coffee', 'src/scripts/**/*.ts', 'src/scripts/**/*.js'],
-    dest: 'dist/js/'
+    src: [
+      "src/scripts/**/*.coffee",
+      "src/scripts/**/*.ts",
+      "src/scripts/**/*.js",
+    ],
+    dest: "dist/js/",
   },
   images: {
-    src: 'src/img/**',
-    dest: 'dist/img/'
-  }
-}
+    src: "src/img/**",
+    dest: "dist/img/",
+  },
+};
 
 // Очистить каталог dist, удалить все кроме изображений
 function clean() {
-  return del(['dist/*', '!dist/img'])
+  return del(["dist/*", "!dist/img"]);
 }
 
 // Обработка html
 function html() {
-  return gulp.src(paths.html.src)
-    .pipe(htmlmin({
-      collapseWhitespace: true
-    }))
-    .pipe(size({
-      showFiles: true
-    }))
+  return gulp
+    .src(paths.html.src)
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      })
+    )
+    .pipe(
+      size({
+        showFiles: true,
+      })
+    )
     .pipe(gulp.dest(paths.html.dest))
-    .pipe(browsersync.stream())
+    .pipe(browsersync.stream());
 }
 
 // Обработка pug
 function pug() {
-  return gulp.src(paths.pug.src)
-    .pipe(gulppug({
-      pretty: true
-    }))
-    .pipe(size({
-      showFiles: true
-    }))
+  return gulp
+    .src(paths.pug.src)
+    .pipe(
+      gulppug({
+        pretty: true,
+      })
+    )
+    .pipe(
+      size({
+        showFiles: true,
+      })
+    )
     .pipe(gulp.dest(paths.pug.dest))
-    .pipe(browsersync.stream())
+    .pipe(browsersync.stream());
 }
 
 // Обработка препроцессоров стилей
 function styles() {
-  return gulp.src(paths.styles.src)
-    .pipe(sourcemaps.init())
-    //.pipe(less())
-    //.pipe(stylus())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(cleanCSS({
-      level: 2
-    }))
-    .pipe(rename({
-      basename: 'style',
-      suffix: '.min'
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(size({
-      showFiles: true
-    }))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browsersync.stream())
+  return (
+    gulp
+      .src(paths.styles.src)
+      .pipe(sourcemaps.init())
+      //.pipe(less())
+      //.pipe(stylus())
+      .pipe(sass().on("error", sass.logError))
+      .pipe(
+        autoprefixer({
+          cascade: false,
+        })
+      )
+      .pipe(
+        cleanCSS({
+          level: 2,
+        })
+      )
+      .pipe(
+        rename({
+          basename: "style",
+          suffix: ".min",
+        })
+      )
+      .pipe(sourcemaps.write("."))
+      .pipe(
+        size({
+          showFiles: true,
+        })
+      )
+      .pipe(gulp.dest(paths.styles.dest))
+      .pipe(browsersync.stream())
+  );
 }
 
 // Обработка Java Script, Type Script и Coffee Script
 function scripts() {
-  return gulp.src(paths.scripts.src)
-    .pipe(sourcemaps.init())
-    //.pipe(coffee({bare: true}))
-    /*
+  return (
+    gulp
+      .src(paths.scripts.src)
+      .pipe(sourcemaps.init())
+      //.pipe(coffee({bare: true}))
+      /*
     .pipe(ts({
       noImplicitAny: true,
       outFile: 'main.min.js'
     }))
     */
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(size({
-      showFiles: true
-    }))
-    .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(browsersync.stream())
+      .pipe(
+        babel({
+          presets: ["@babel/env"],
+        })
+      )
+      .pipe(uglify())
+      .pipe(concat("main.min.js"))
+      .pipe(sourcemaps.write("."))
+      .pipe(
+        size({
+          showFiles: true,
+        })
+      )
+      .pipe(gulp.dest(paths.scripts.dest))
+      .pipe(browsersync.stream())
+  );
 }
 
 // Сжатие изображений
 function img() {
-  return gulp.src(paths.images.src)
+  return gulp
+    .src(paths.images.src)
     .pipe(newer(paths.images.dest))
-    .pipe(imagemin({
-      progressive: true
-    }))
-    .pipe(size({
-      showFiles: true
-    }))
-    .pipe(gulp.dest(paths.images.dest))
+    .pipe(
+      imagemin({
+        progressive: true,
+        optimizationLevel: 3,
+      })
+    )
+    .pipe(
+      size({
+        showFiles: true,
+      })
+    )
+    .pipe(gulp.dest(paths.images.dest));
 }
 
 // Отслеживание изменений в файлах и запуск лайв сервера
 function watch() {
   browsersync.init({
     server: {
-      baseDir: "./dist"
-    }
-  })
-  gulp.watch(paths.pug.dest).on('change', browsersync.reload)
-  gulp.watch(paths.html.dest).on('change', browsersync.reload)
-  gulp.watch(paths.html.src, html)
-  gulp.watch(paths.pug.src, pug)
-  gulp.watch(paths.styles.src, styles)
-  gulp.watch(paths.scripts.src, scripts)
-  gulp.watch(paths.images.src, img)
+      baseDir: "./dist",
+    },
+  });
+  gulp.watch(paths.pug.dest).on("change", browsersync.reload);
+  gulp.watch(paths.html.dest).on("change", browsersync.reload);
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.pug.src, pug);
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.images.src, img);
 }
 
 // Таски для ручного запуска с помощью gulp clean, gulp html и т.д.
-exports.clean = clean
-exports.html = html
-exports.pug = pug
-exports.styles = styles
-exports.scripts = scripts
-exports.img = img
-exports.watch = watch
+exports.clean = clean;
+exports.html = html;
+exports.pug = pug;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.img = img;
+exports.watch = watch;
 
 // Таск, который выполняется по команде gulp
-exports.default = gulp.series(clean, /*html*/ gulp.parallel(pug, styles, scripts, img), watch)
+exports.default = gulp.series(
+  clean,
+  /*html*/ gulp.parallel(pug, styles, scripts, img),
+  watch
+);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,285 +1,250 @@
 // form placeholder
-const formItems = document.querySelectorAll('.form-item__field');
+const formItems = document.querySelectorAll(".form-item__field");
 
 for (let item of formItems) {
-    const thisParent = item.closest('.form-item');
-    const thisPlaceholder = thisParent.querySelector('.form-item__placeholder');
-    // Если инпут в фокусе
-    item.addEventListener('focus', function () {
-        thisPlaceholder.classList.add('active');
-    });
+  const thisParent = item.closest(".form-item");
+  const thisPlaceholder = thisParent.querySelector(".form-item__placeholder");
+  // Если инпут в фокусе
+  item.addEventListener("focus", function () {
+    thisPlaceholder.classList.add("active");
+  });
 
-    // Если инпут теряет фокус
-    item.addEventListener('blur', function () {
-        if (item.value.length > 0) {
-            thisPlaceholder.classList.add('active');
-        } else {
-            thisPlaceholder.classList.remove('active');
-        }
-    })
-};
+  // Если инпут теряет фокус
+  item.addEventListener("blur", function () {
+    if (item.value.length > 0) {
+      thisPlaceholder.classList.add("active");
+    } else {
+      thisPlaceholder.classList.remove("active");
+    }
+  });
+}
 // Atttach files
 window.onload = () => {
-    const uploadFile = document.getElementById("form__upload-file");
-    const uploadBtn = document.getElementById("form__upload-btn");
-    const uploadText = document.getElementById("form__upload-text");
+  const uploadFile = document.getElementById("form__upload-file");
+  const uploadBtn = document.getElementById("form__upload-btn");
+  const uploadText = document.getElementById("form__upload-text");
 
-    // Иммитируем нажатие по нашей скрытой кнопке закгрузке файлов
-    if (uploadBtn) {
-        uploadBtn.addEventListener("click", function () {
-            uploadFile.click();
-        });
-    }
-    // Добавляем событие загрузки файла
-    if (uploadFile) {
-        uploadFile.addEventListener("change", function () {
-            if (uploadFile.value) {
-                uploadText.innerText = uploadFile.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
-            } else {
-                uploadText.innerText = "Файл не выбран";
-            }
-        });
-    }
+  // Иммитируем нажатие по нашей скрытой кнопке закгрузке файлов
+  if (uploadBtn) {
+    uploadBtn.addEventListener("click", function () {
+      uploadFile.click();
+    });
+  }
+  // Добавляем событие загрузки файла
+  if (uploadFile) {
+    uploadFile.addEventListener("change", function () {
+      if (uploadFile.value) {
+        uploadText.innerText = uploadFile.value.match(
+          /[\/\\]([\w\d\s\.\-(\)]+)$/
+        )[1];
+      } else {
+        uploadText.innerText = "Файл не выбран";
+      }
+    });
+  }
 };
 
 // pop-up
-let popupBg = document.querySelector('#popup__bg');
-let popup = document.querySelector('.popup');
-let openPopupButtons = document.querySelectorAll('#popup__open');
-let closePopupButton = document.querySelector('.popup__close');
-let body = document.querySelector('body');
-
+let popupBg = document.querySelector("#popup__bg");
+let popup = document.querySelector(".popup");
+let openPopupButtons = document.querySelectorAll("#popup__open");
+let closePopupButton = document.querySelector(".popup__close");
+let body = document.querySelector("body");
 
 openPopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        popupBg.classList.add('active');
-        popup.classList.add('popup__active');
-        body.classList.add('locked');
-    })
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    popupBg.classList.add("active");
+    popup.classList.add("popup__active");
+    body.classList.add("locked");
+  });
 });
-closePopupButton.addEventListener('click', () => {
-    popupBg.classList.remove('active');
-    popup.classList.remove('popup__active');
-    body.classList.remove('locked');
+closePopupButton.addEventListener("click", () => {
+  popupBg.classList.remove("active");
+  popup.classList.remove("popup__active");
+  body.classList.remove("locked");
 });
-document.addEventListener('click', (e) => {
-    if (e.target === popupBg) {
-        popupBg.classList.remove('active');
-        popup.classList.remove('popup__active');
-        body.classList.remove('locked');
-    }
+document.addEventListener("click", (e) => {
+  if (e.target === popupBg) {
+    popupBg.classList.remove("active");
+    popup.classList.remove("popup__active");
+    body.classList.remove("locked");
+  }
 });
 
 // Accordion index
-const accordionTitles = document.querySelectorAll('.accordion__title');
+const accordionTitles = document.querySelectorAll(".accordion__title");
 const tabIndicator = document.querySelector("#tab__indicator");
 
 accordionTitles.forEach((accordionTitle) => {
-    accordionTitle.addEventListener('click', () => {
-        const height = accordionTitle.nextElementSibling.scrollHeight;
-        accordionTitle.classList.toggle("accordion__title-active");
-        if (accordionTitle.classList.contains('accordion__title-active')) {
-            accordionTitle.nextElementSibling.style.maxHeight = `${height}px`;
-        } else {
-            accordionTitle.nextElementSibling.style.maxHeight = "0px";
-        }
-    })
+  accordionTitle.addEventListener("click", () => {
+    const height = accordionTitle.nextElementSibling.scrollHeight;
+    accordionTitle.classList.toggle("accordion__title-active");
+    if (accordionTitle.classList.contains("accordion__title-active")) {
+      accordionTitle.nextElementSibling.style.maxHeight = `${height}px`;
+    } else {
+      accordionTitle.nextElementSibling.style.maxHeight = "0px";
+    }
+  });
 });
 
-
 // Tabs
-let tabNavItem = document.querySelectorAll('.tabs__nav-item');
-let tabContentItem = document.querySelectorAll('.tabs__content');
+let tabNavItem = document.querySelectorAll(".tabs__nav-item");
+let tabContentItem = document.querySelectorAll(".tabs__content");
 
 tabNavItem.forEach(function (elem) {
-    elem.addEventListener('click', activeTab);
-})
+  elem.addEventListener("click", activeTab);
+});
 
 function activeTab() {
-    tabNavItem.forEach(function (elem) {
-        elem.classList.remove('active');
-    })
-    this.classList.add('active');
-    let tabName = this.getAttribute('data-tab');
+  tabNavItem.forEach(function (elem) {
+    elem.classList.remove("active");
+  });
+  this.classList.add("active");
+  let tabName = this.getAttribute("data-tab");
 
-    activeTabContent(tabName);
-
+  activeTabContent(tabName);
 }
 
 function activeTabContent(tabName) {
-    tabContentItem.forEach(function (item) {
-        if (item.classList.contains(tabName)) {
-            item.classList.add('active');
-        } else {
-            item.classList.remove('active');
-        }
-    })
-};
+  tabContentItem.forEach(function (item) {
+    if (item.classList.contains(tabName)) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+}
+// Carousel
+const carouselImages = document.querySelectorAll(".carousel__item");
+const carouselLine = document.querySelector(".carousel__line");
+const carouselDots = document.querySelectorAll(".carousel__dot");
+const carouselBtnNext = document.querySelector(".carousel__arrow-next");
+const carouselBtnPrev = document.querySelector(".carousel__arrow-prev");
+
+let carouselCount = 0;
+let carouselWidth;
+
+// Адаптивность слайдера
+window.addEventListener("resize", showCarousel);
+
+// Кнопки слайдов вперед и назад
+carouselBtnNext.addEventListener("click", nextBtn);
+carouselBtnPrev.addEventListener("click", prevBtn);
+
+function showCarousel() {
+  carouselWidth = document.querySelector(".carousel__wrapper").offsetWidth;
+  carouselLine.style.width = carouselWidth * carouselImages.length + "px";
+  carouselImages.forEach((item) => (item.style.width = carouselWidth + "px"));
+
+  rollcarousel();
+}
+
+showCarousel();
+
+// Перелистывает слад вперед
+function nextBtn() {
+  carouselCount++;
+  if (carouselCount >= carouselImages.length) carouselCount = 0;
+
+  rollcarousel();
+  thisCarousel(carouselCount);
+}
+
+// Перелистывает слад назад
+function prevBtn() {
+  carouselCount--;
+  if (carouselCount < 0) carouselCount = carouselImages.length - 1;
+
+  rollcarousel();
+  thisCarousel(carouselCount);
+}
+
+// Задает шаг перемещения сладов
+function rollcarousel() {
+  carouselLine.style.transform = `translateX(${
+    -carouselCount * carouselWidth
+  }px)`;
+}
+
+// Указывает какой слайд по счету активен
+
+function thisCarousel(index) {
+  carouselDots.forEach((item) => item.classList.remove("carousel__dot-active"));
+  carouselDots[index].classList.add("carousel__dot-active");
+}
+
+// Вешаем клик на dot
+carouselDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    carouselCount = index;
+    rollcarousel();
+    thisCarousel(carouselCount);
+  });
+});
 
 // Slider
-let slider = document.getElementById('slider'),
-    sliderItems = document.getElementById('slides'),
-    prev = document.getElementById('prev'),
-    next = document.getElementById('next');
+const sliderImages = document.querySelectorAll(".slider__item");
+const sliderLine = document.querySelector(".slider__line");
+const sliderDots = document.querySelectorAll(".slider__dot");
+const sliderBtnNext = document.querySelector(".slider__arrow-next");
+const sliderBtnPrev = document.querySelector(".slider__arrow-prev");
 
-function slide(wrapper, items, prev, next) {
-    let posX1 = 0,
-        posX2 = 0,
-        posInitial,
-        posFinal,
-        threshold = 100,
-        slides = items.getElementsByClassName('slider__item'),
-        slidesLength = slides.length,
-        slideSize = items.getElementsByClassName('slider__item')[0].offsetWidth,
-        firstSlide = slides[0],
-        lastSlide = slides[slidesLength - 1],
-        cloneFirst = firstSlide.cloneNode(true),
-        cloneLast = lastSlide.cloneNode(true),
-        index = 0,
-        allowShift = true;
+let sliderCount = 0;
+let sliderWidth;
 
-    // Clone first and last slide
-    items.appendChild(cloneFirst);
-    items.insertBefore(cloneLast, firstSlide);
-    wrapper.classList.add('loaded');
+// Адаптивность слайдера
+window.addEventListener("resize", showSlide);
 
-    // Mouse events
-    items.onmousedown = dragStart;
+// Кнопки слайдов вперед и назад
+sliderBtnNext.addEventListener("click", nextSlide);
+sliderBtnPrev.addEventListener("click", prevSlide);
 
-    // Touch events
-    items.addEventListener('touchstart', dragStart);
-    items.addEventListener('touchend', dragEnd);
-    items.addEventListener('touchmove', dragAction);
+function showSlide() {
+  sliderWidth = document.querySelector(".slider__wrapper").offsetWidth;
+  sliderLine.style.width = sliderWidth * sliderImages.length + "px";
+  sliderImages.forEach((item) => (item.style.width = sliderWidth + "px"));
 
-    // Click events
-    prev.addEventListener('click', function () {
-        shiftSlide(-1)
-    });
-    next.addEventListener('click', function () {
-        shiftSlide(1)
-    });
-
-    // Transition events
-    items.addEventListener('transitionend', checkIndex);
-
-    function dragStart(e) {
-        e = e || window.event;
-        e.preventDefault();
-        posInitial = items.offsetLeft;
-
-        if (e.type == 'touchstart') {
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX1 = e.clientX;
-            document.onmouseup = dragEnd;
-            document.onmousemove = dragAction;
-        }
-    }
-
-    function dragAction(e) {
-        e = e || window.event;
-
-        if (e.type == 'touchmove') {
-            posX2 = posX1 - e.touches[0].clientX;
-            posX1 = e.touches[0].clientX;
-        } else {
-            posX2 = posX1 - e.clientX;
-            posX1 = e.clientX;
-        }
-        items.style.left = (items.offsetLeft - posX2) + "px";
-    }
-
-    function dragEnd(e) {
-        posFinal = items.offsetLeft;
-        if (posFinal - posInitial < -threshold) {
-            shiftSlide(1, 'drag');
-        } else if (posFinal - posInitial > threshold) {
-            shiftSlide(-1, 'drag');
-        } else {
-            items.style.left = (posInitial) + "px";
-        }
-
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-
-    function shiftSlide(dir, action) {
-        items.classList.add('shifting');
-
-        if (allowShift) {
-            if (!action) {
-                posInitial = items.offsetLeft;
-            }
-
-            if (dir == 1) {
-                items.style.left = (posInitial - slideSize) + "px";
-                index++;
-            } else if (dir == -1) {
-                items.style.left = (posInitial + slideSize) + "px";
-                index--;
-            }
-        };
-
-        allowShift = false;
-    }
-
-    function checkIndex() {
-        items.classList.remove('shifting');
-
-        if (index == -1) {
-            items.style.left = -(slidesLength * slideSize) + "px";
-            index = slidesLength - 1;
-        }
-
-        if (index == slidesLength) {
-            items.style.left = -(1 * slideSize) + "px";
-            index = 0;
-        }
-
-        allowShift = true;
-    }
+  rollSlider();
 }
-slide(slider, sliderItems, prev, next);
 
-// Carousel
+showSlide();
 
-// Select the carousel you'll need to manipulate and the buttons you'll add events to
-const carousel = document.querySelector("[data-target='carousel']");
-const card = carousel.querySelector("[data-target='card']");
-const leftButton = document.querySelector("[data-action='carouselLeft']");
-const rightButton = document.querySelector("[data-action='carouselRight']");
+// Перелистывает слад вперед
+function nextSlide() {
+  sliderCount++;
+  if (sliderCount >= sliderImages.length) sliderCount = 0;
 
-// Prepare to limit the direction in which the carousel can slide, 
-// and to control how much the carousel advances by each time.
-// In order to slide the carousel so that only three cards are perfectly visible each time,
-// you need to know the carousel width, and the margin placed on a given card in the carousel
-const carouselWidth = carousel.offsetWidth;
-const cardStyle = card.currentStyle || window.getComputedStyle(card)
-const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+  rollSlider();
+  thisSlide(sliderCount);
+}
 
-// Count the number of total cards you have
-const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+// Перелистывает слад назад
+function prevSlide() {
+  sliderCount--;
+  if (sliderCount < 0) sliderCount = sliderImages.length - 1;
 
-// Define an offset property to dynamically update by clicking the button controls
-// as well as a maxX property so the carousel knows when to stop at the upper limit
-let offset = 0;
-const maxX = -((cardCount / 3) * carouselWidth +
-    (cardMarginRight * (cardCount / 3)) -
-    carouselWidth - cardMarginRight);
+  rollSlider();
+  thisSlide(sliderCount);
+}
 
+// Задает шаг перемещения сладов
+function rollSlider() {
+  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
 
-// Add the click events
-leftButton.addEventListener("click", function () {
-    if (offset !== 0) {
-        offset += carouselWidth + cardMarginRight;
-        carousel.style.transform = `translateX(${offset}px)`;
-    }
-})
+// Указывает какой слайд по счету активен
 
-rightButton.addEventListener("click", function () {
-    if (offset !== maxX) {
-        offset -= carouselWidth + cardMarginRight;
-        carousel.style.transform = `translateX(${offset}px)`;
-    }
-})
+function thisSlide(index) {
+  sliderDots.forEach((item) => item.classList.remove("slider__dot-active"));
+  sliderDots[index].classList.add("slider__dot-active");
+}
+
+// Вешаем клик на dot
+sliderDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    sliderCount = index;
+    rollSlider();
+    thisSlide(sliderCount);
+  });
+});
